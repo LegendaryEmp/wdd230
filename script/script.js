ab3449404b0b87517be0d9e1284ffd46e8ef886f
LegendaryEmp <jacobokwusi@gmail.com>
2023-01-26 01:08:28 +0100

    Added last modified

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
     //tolocaleDateString
-    const options = {weekday: "long", day: "numeric", month: "long", year: "numeric"};
-    document.getElementById("datetime").textContent = new Date().toLocaleDateString("en-US", options);
+    // const options = {weekday: "long", day: "numeric", month: "long", year: "numeric"};
+    // document.getElementById("datetime").textContent = new Date().toLocaleDateString("en-US", options);
+
+    document.getElementById("datetime").innerHTML = document.lastModified;
